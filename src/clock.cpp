63219a2b68526627bd187c6abb2b7c9a67b6e725
Andrew Starritt <andrew.starritt@gmail.com>
2022-05-28 18:17:28 +1000

    Add new clock module files

---
+++ src/clock.cpp
@@ -0,0 +1,100 @@
+/* clock.cpp
+ *
+ * Locus 16 Emulator clock module, part of the Locus 16 Emulator.
+ *
+ * Copyright (c) 2022 Anddrew Starritt
+ *
+ * The Locus 16 Emulator is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * The Locus 16 Emulator is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the Locus 16 Emulator.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Contact details:
+ * andrew.starritt@gmail.com
+ */
+
+#include "clock.h"
+
+using namespace L16E;
+
+//------------------------------------------------------------------------------
+//
+Clock::Clock (DataBus* const dataBus) :
+   DataBus::Device (dataBus, 0x7C00, 0x7C04, "Clock", false)
+{
+   this->isRunning = false;
+   this->interval = 0;
+   this->countDown = 0.0;
+   this->interruptPending = false;
+}
+
+//------------------------------------------------------------------------------
+//
+Clock::~Clock()  { }   // place holder
+
+//------------------------------------------------------------------------------
+//
+void Clock::executeCycle()
+{
+   if (this->isRunning) {
+      // a typlical ALP instruction is 2.25 uSec
+      // If more than one active device, we should adjust this.
+      //
+      this->countDown -= 2.25;
+      if (this->countDown <= 0.0) {
+         this->interruptPending = true;
+         this->countDown += 1000.0 * this->interval;  // reset count down.
+         if (this->countDown < 10) this->countDown = 10;
+      }
+   }
+}
+
+//------------------------------------------------------------------------------
+//
+bool Clock::testAndClearInterruptPending ()
+{
+   bool result = this->interruptPending;
+   this->interruptPending = false;
+   return result;
+}
+
+//------------------------------------------------------------------------------
+//
+Int16 Clock::getWord(const Int16 addr) const
+{
+   Int16 result;
+
+   if (addr == 0x7C00) {
+      result = this->isRunning ? 1 : 0;
+   } else if (addr == 0x7C02) {
+      result = Int16 (this->interval);
+   } else {
+      result = 0xFFFF;
+   }
+
+   return result;
+}
+
+//------------------------------------------------------------------------------
+//
+void Clock::setWord(const Int16 addr, const Int16 value)
+{
+   if (addr == 0x7C00) {
+      this->isRunning = value & 1;
+   } else if (addr == 0x7C02) {
+      this->interval = (value >= 0) ? value : value + 0x010000;
+      this->countDown = 1000.0 * this->interval;  // reset count down.
+      if (this->countDown < 10) this->countDown = 10;
+   }
+}
+
+// end
+
---
+++ src/clock.h
@@ -0,0 +1,64 @@
+/* clock.h
+ *
+ * Clock module, part of the Locus 16 Emulator.
+ *
+ * Copyright (c) 2022  Andrew C. Starritt
+ *
+ * The Locus 16 Emulator is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * The Locus 16 Emulator is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License and
+ * the Lesser GNU General Public License along with the Locus 16 Emulator.
+ * If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Contact details:
+ * andrew.starritt@gmail.com
+ * PO Box 3118, Prahran East, Victoria 3181, Australia.
+ */
+
+#ifndef L16E_CLOCK_H
+#define L16E_CLOCK_H
+
+#include "data_bus.h"
+
+namespace L16E {
+
+// This emulates a clock.
+//
+class Clock : public DataBus::Device {
+public:
+   // Clock address is 0x7C00 to 0x7C04
+   // 0x7C00 status/control register
+   // bit 0: write 0 stop clock
+   // bit 0: write 1 to start clock
+   // bit 0; read to find status - running(1) or stopped (0)
+   // 0x7C02 interrupt interval (in simulated mSec based on 2.25 uSec/instruction)
+   // Value written treated as an unsigned 16 bit integer.
+   //
+   explicit Clock (DataBus* const dataBus);
+   virtual ~Clock();
+
+   bool testAndClearInterruptPending ();
+
+   Int16 getWord(const Int16 addr) const;
+   void setWord(const Int16 addr, const Int16  value);
+
+   void executeCycle();   // called prior to each instruction execution
+
+private:
+   bool isRunning;
+   Int16 interval;      // in emulated mSec
+   double countDown;    // in emulated uSec
+   volatile bool interruptPending;
+};
+
+}
+
+#endif // L16E_CLOCK_H
